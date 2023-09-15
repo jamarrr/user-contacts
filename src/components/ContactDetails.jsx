73e6804a028b2908bcd02a7e6b2b9c1975e63c13
John Mar Abarientos <johnmaraabarientos@gmail.com>
2023-09-16 01:35:49 +0800

    Added data fetching for displaying all contacts and contact details

--- src/components/ContactDetails.jsx
+++ src/components/ContactDetails.jsx
@@ -1,7 +1,23 @@
 /* eslint-disable react/prop-types */
 import styles from "./ContactDetails.module.css";
+import useSwr from "swr";
 
-export default function ContactDetails({ setSelectedContactId }) {
+export default function ContactDetails({ selectedId, setSelectedContactId }) {
+  /**
+   * Fetcher for contact details depending on the selected id passed
+   * @param {*} id
+   * @returns contact details obj
+   */
+  const fetchContactDetails = async (id) => {
+    const contactDetails = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    return contactDetails.json();
+  };
+
+  // swr request for contact details
+  const { data: contactDetails } = useSwr(selectedId, fetchContactDetails);
+
   return (
     <>
       <div className={styles.action}>
@@ -13,33 +29,32 @@ export default function ContactDetails({ setSelectedContactId }) {
       <div className={styles["name-container"]}>
         <div className={styles.section}>
           <h3 className={styles["row-label"]}>Full name</h3>
-          <h4>John Mar Abarientos</h4>
+          <h4>{contactDetails?.name}</h4>
         </div>
         <div className={styles.section}>
           <h3 className={styles["row-label"]}>Username</h3>
-          <h4>Migibuy</h4>
+          <h4>{contactDetails?.username}</h4>
         </div>
       </div>
 
       <div className={styles.section}>
         <h3 className={styles["row-label"]}>Address</h3>
-        <h4>Quisao, Pililla, Rizal, Philippines</h4>
+        <h4>{`${contactDetails?.address?.suite}, ${contactDetails?.address?.street}, ${contactDetails?.address?.city} - Zipcode: ${contactDetails?.address?.zipcode}`}</h4>
       </div>
 
       <div className={styles.section}>
         <h3 className={styles["row-label"]}>Contact number</h3>
-        <h4>123456789</h4>
+        <h4>{contactDetails?.phone}</h4>
       </div>
 
       <div className={styles.section}>
         <h3 className={styles["row-label"]}>Website</h3>
-        <h4>https://google.com</h4>
+        <h4>{contactDetails?.website}</h4>
       </div>
 
       <div className={styles.section}>
         <h3 className={styles["row-label"]}>Company</h3>
-        <h4>Google</h4>
-        <h5>Google</h5>
+        <h4>{contactDetails?.company?.name}</h4>
       </div>
     </>
   );
--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -3,11 +3,11 @@ import styles from "./ContactInfo.module.css";
 
 export default function ContactInfo({ contact, setSelectedContactId }) {
   return (
-    <tr className={styles.tr} onClick={() => setSelectedContactId(contact.id)}>
-      <td className={styles.td}>{contact.name}</td>
-      <td className={styles.td}>{contact.username}</td>
-      <td className={styles.td}>{contact.email}</td>
-      <td className={styles.td}>{contact.phone}</td>
+    <tr className={styles.tr} onClick={() => setSelectedContactId(contact?.id)}>
+      <td className={styles.td}>{contact?.name}</td>
+      <td className={styles.td}>{contact?.username}</td>
+      <td className={styles.td}>{contact?.email}</td>
+      <td className={styles.td}>{contact?.phone}</td>
     </tr>
   );
 }
