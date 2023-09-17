fc40d9a4f5d29a7a898a757e228cf899df5f4156
John Mar Abarientos <johnmaraabarientos@gmail.com>
2023-09-17 13:41:20 +0800

    Made ui layout responsive

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,8 +1,19 @@
 /* eslint-disable react/prop-types */
 import styles from "./ContactInfo.module.css";
 
-export default function ContactInfo({ contact, setSelectedContactId }) {
-  return (
+export default function ContactInfo({
+  isMobile,
+  contact,
+  setSelectedContactId,
+}) {
+  return isMobile ? (
+    <div
+      className={styles.contact}
+      onClick={() => setSelectedContactId(contact?.id)}
+    >
+      {contact?.name ?? contact?.username}
+    </div>
+  ) : (
     <tr className={styles.tr} onClick={() => setSelectedContactId(contact?.id)}>
       <td className={styles.td}>{contact?.name}</td>
       <td className={styles.td}>{contact?.username}</td>
